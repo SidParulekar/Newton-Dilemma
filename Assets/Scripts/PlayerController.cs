08ffdaa89da99c21475b61af8a720514a060b010
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-09-24 19:28:43 +0530

    Implemented Player animations and added Collisions and Gravity effect
    Made changes to the PlayerController script to introduce gravitational effect.
    
    Added Character Controller component to the Player object to include collision effect.
    
    Set up the animations and transitions between each in the animator.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,32 +7,76 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float rotationSpeed = 500f;
 
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] Vector3 groundCheckOffset;
+    [SerializeField] LayerMask groundLayer;
+
+    bool isGrounded;
+
+    float ySpeed;
+
     CameraController cameraController;
+
+    Animator animator;
+
+    CharacterController characterController;
 
     Quaternion targetRotation;
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();  
     }
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        float moveAmount = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+        float moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
 
         var moveInput = (new Vector3(horizontal, 0, vertical).normalized);
 
         var moveDir = cameraController.PlanarRotation * moveInput;
 
-        if (moveAmount > 0)
+        GroundCheck();
+
+        if(isGrounded)
         {
-            transform.position -= moveDir * moveSpeed * Time.deltaTime;
+            ySpeed = -0.5f;
+            animator.SetBool("onGround", true);
+        }
+
+        else
+        {
+            animator.SetBool("onGround", false);
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * moveSpeed * -1;
+        velocity.y = ySpeed;
+
+        characterController.Move( velocity * Time.deltaTime);
+
+        if (moveAmount > 0)
+        {          
             targetRotation = Quaternion.LookRotation(moveDir*-1);
         }
        
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        
 
+        animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
+
+    }
+
+    void GroundCheck()
+    {
+       isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
